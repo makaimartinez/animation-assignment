e7ff8ff7bdc115767d00494b2d2a856b208ca9f2
makai <makaimartinez.edu@gmail.com>
2024-01-28 19:54:20 -0800

    adding movement

--- link.js
+++ link.js
@@ -3,7 +3,10 @@ class Link {
     constructor() {
         this.x = 100;
         this.y = 100;
-        this.speed = 100;
+
+        this.facing = 0; // 0 = right, 1 = left, 2 = up, 3 = down
+        this.velocity = { x: 0, y: 0 };
+
 
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/linksprites.png");
 
@@ -55,46 +58,78 @@ class Link {
     }
 
     update() {
+        const TICK = gameEngine.clockTick;
+        const MIN_WALK = 30;
+        const MAX_WALK = 160;
+        const ACC_WALK = 40;
+
         // update velocity
-        this.x += this.speed * gameEngine.clockTick;
-        if (this.x > 800) {
-            this.x = 0;
+        if (Math.abs(this.velocity.x) < MIN_WALK) { //if not moving, check for button then add movement
+            this.velocity.x = 0;
+            this.state = 0;
+            if(gameEngine.right) {
+                this.velocity.x += ACC_WALK;
+            }
+            if(gameEngine.left) {
+                this.velocity.x -= ACC_WALK;
+            }
+        } else if (Math.abs(this.velocity.x) >= MIN_WALK) { // if greater than min_walk, check if player wants to speed up or slow down
+            if(this.facing == false) { //facing right
+                if (gameEngine.right && !gameEngine.left && !gameEngine.down) {
+                    this.velocity.x += ACC_WALK * TICK;
+                } else if (gameEngine.left && !gameEngine.right && !gameEngine.down) {
+                    this.velocity.x -= DEC_SKID * TICK;
+                } else {
+                    this.velocity.x -= DEC_REL * TICK;
+                }
+            } else {
+                if (gameEngine.left && !gameEngine.right && !gameEngine.down) {
+                    this.velocity.x -= ACC_WALK * TICK;
+                } else if (gameEngine.right && !gameEngine.left && !gameEngine.down) {
+                    this.velocity.x += DEC_SKID * TICK;
+                } else {
+                    this.velocity.x += DEC_REL * TICK;
+                }
+            }
         }
+
         // update position
+
+        // update direction 
+        if (this.velocity.x < 0) this.facing = true;
+        if (this.velocity.x > 0) this.facing = false;
+
+        // update speed
+        this.x += this.velocity.x * TICK * 2;
+        this.y += this.velocity.y * TICK * 2;
     }
 
     draw() {
         let scale = 3;
         let tick = gameEngine.clockTick;
+
+
+
+        
         // right walk
         this.animations[0][0].drawFrame(tick, this.x, this.y, scale);
-        this.animations[0][1].drawFrame(tick, this.x + 100, this.y, scale);
-        // // left walk
+        // this.animations[0][1].drawFrame(tick, this.x + 100, this.y, scale);
+
+        // left walk
         // this.animations[1][0].drawFrame(tick, this.x, this.y + 100, scale);
         // this.animations[1][1].drawFrame(tick, this.x + 100, this.y + 100, scale);
+        
         // walk up
-        this.animations[2][0].drawFrame(tick, 0, this.y + 200, scale);
-        this.animations[2][1].drawFrame(tick, 0 + 100, this.y + 200, scale);
+        // this.animations[2][0].drawFrame(tick, 0, this.y + 200, scale);
+        // this.animations[2][1].drawFrame(tick, 0 + 100, this.y + 200, scale);
+        
         // walk down
-        this.animations[3][0].drawFrame(tick, 0, this.y + 300, scale);
-        this.animations[3][1].drawFrame(tick, 0 + 100, this.y + 300, scale);
+        // this.animations[3][0].drawFrame(tick, 0, this.y + 300, scale);
+        // this.animations[3][1].drawFrame(tick, 0 + 100, this.y + 300, scale);
 
         // HitBox?
-        //ctx.strokeStyle = "Green"
-        //ctx.strokeRect(this.x, this.y,  this.width * scale, this.height * scale);
-
-        // reverse image
-        // ctx.save();
-        // ctx.scale(-1, 1);
-        // ctx.drawImage(ASSET_MANAGER.getAsset("./sprites/linksprites.png"),
-        // sx, sy, this.width, this.height,
-        // - this.x - (this.width * scale), this.y, this.width * scale, this.height * scale);
-        // ctx.restore();
-
-        // draws links sprite #1
-        // ctx.drawImage(ASSET_MANAGER.getAsset("./sprites/linksprites.png"),
-        //     sx, sy, this.width, this.height,
-        //     this.x, this.y, this.width * scale, this.height * scale);
+        ctx.strokeStyle = "Green"
+        ctx.strokeRect(this.x, this.y,  this.width * scale, this.height * scale);
     }
 
 }
